c5468b94952e84baaa79e4406e6fb24837ba6d87
vkani <kani.banu07@gmail.com>
2022-03-05 22:27:06 +0530

    34rth

---
+++ src/org/comp/Computer.java
@@ -0,0 +1,7 @@
+package org.comp;
+
+public class Computer {
+public void computerMode() {
+	System.out.println("Computer Mode");
+}
+}
---
+++ src/org/comp/Desktop.java
@@ -0,0 +1,12 @@
+package org.comp;
+
+public class Desktop extends Computer {
+public void desktopSize() {
+	System.out.println("Desktop Size");
+}
+public static void main(String[] args) {
+	Desktop d=new Desktop();
+	d.desktopSize();
+	d.computerMode();
+}
+}
